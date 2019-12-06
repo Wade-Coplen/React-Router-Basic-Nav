15a12f94f23bedce84ca9b972634b72489e4dd75
Your Real Name Here <Your Email Address Here>
2019-12-05 21:29:03 -0700

    2. Code is broken

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to="/"/>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to="/about"/>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to="/contact"/>
+
         </div>
       </div>
     </div>
